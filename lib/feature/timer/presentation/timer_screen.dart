ed234b55e041471e4a608e629cae5c7608f841c2
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-06-12 21:24:43 +0545

    folder structure and packages added

---
+++ lib/feature/timer/presentation/timer_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TimerScreen extends StatefulWidget {
+  const TimerScreen({super.key});
+
+  @override
+  State<TimerScreen> createState() => _TimerScreenState();
+}
+
+class _TimerScreenState extends State<TimerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Timer Screen'),
+      ),
+      body: const Column(
+        children: [],
+      ),
+    );
+  }
+}
