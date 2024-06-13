d60bfb529d8689337161ee9b835fa21d84dce5f9
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-06-13 15:54:30 +0545

    camera setup for macos

--- lib/feature/timer/presentation/timer_screen.dart
+++ lib/feature/timer/presentation/timer_screen.dart
@@ -1,3 +1,4 @@
+import 'package:camera_macos/camera_macos.dart';
 import 'package:flutter/material.dart';
 
 class TimerScreen extends StatefulWidget {
@@ -8,14 +9,41 @@ class TimerScreen extends StatefulWidget {
 }
 
 class _TimerScreenState extends State<TimerScreen> {
+  final GlobalKey cameraKey = GlobalKey();
+  late CameraMacOSController macOSController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Timer Screen'),
       ),
-      body: const Column(
-        children: [],
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Start Timer'),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 200,
+              width: 200,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: CameraMacOSView(
+                  key: cameraKey,
+                  fit: BoxFit.fill,
+                  cameraMode: CameraMacOSMode.video,
+                  onCameraInizialized: (CameraMacOSController controller) {
+                    setState(() {
+                      macOSController = controller;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
